ae1da3e3f554d3bbd329759e695ff4f93ed026ff
Hafiz Abdullah Yasir <hafizabdullahyasir.7@gmail.com>
2025-07-09 18:13:46 +0500

    added functionality to memoization screen added two use state hooks and functions

--- src/screens/MemoizationScreen.tsx
+++ src/screens/MemoizationScreen.tsx
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Button, FlatList } from "react-native";  
+import { View, Text, StyleSheet, Button, FlatList } from "react-native";
 import React, { useState, useCallback, useMemo, useEffect } from 'react';
 import { Logger } from '../utils/Logger';
 
@@ -8,33 +8,53 @@ const usersData = [
     { id: '1', name: 'Alice', status: 'online' },
     { id: '2', name: 'Bob', status: 'offline' },
     { id: '3', name: 'Charlie', status: 'online' },
-  ];
+];
 
+const UserItem = React.memo(({ name }: { name: string }) => {
 
+    Logger.info(`Rendering UserItem for ${name}`);
+    return (
+        <View>
+            <Text>{name}</Text>
+        </View>
+    )
+});
 
 
 
 export default function MemoizationScreen() {
+ const [counter, setCounter] = useState(0);
+ const [users, setUsers] = useState(usersData);
+
+
+
+
+    const onlineUsers = useMemo(()=>{
+        Logger.debug(`Calculating online users`);
+        return users.filter(user=> user.status === 'online')
+}, [users])
+
+const handlePress = useCallback(()=>{
+    setCounter(prev => prev + 1)
+},[])
 
 
 
 
 
 
-
-
-return (
-      <View>
-       <Text>Counter: {counter}</Text>
-       <Button title="Increment" onPress={increment} />
-       <Text>Online Users</Text>
-       <FlatList/>
-      </View>
+    return (
+        <View>
+            <Text>Counter: {counter}</Text>
+            <Button title="Increment" onPress={handlePress} />
+            <Text>Online Users</Text>
+            <FlatList data={onlineUsers} keyExtractor={item => item.id} renderItem={({item}) => <UserItem name={item.name} />} />
+        </View>
 
 
 
 
-)
+    )
 
 
 
