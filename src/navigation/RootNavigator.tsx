c45aa4980189208b352a2e9abf9f46a6eb7a9b2e
Hafiz Abdullah Yasir <hafizabdullahyasir.7@gmail.com>
2025-07-10 16:20:23 +0500

    added stack screen for counterscreen

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -3,6 +3,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import HomeScreen from "../screens/HomeScreen";
 import DetailsScreen from "../screens/DetailsScreen";
 import MemoizationScreen from "../screens/MemoizationScreen";
+import CounterScreen from "../screens/CounterScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ export default function RootNavigator() {
             <Stack.Screen name="Home" component={HomeScreen} />
             <Stack.Screen name="Details" component={DetailsScreen} />
             <Stack.Screen name="Memoization" component={MemoizationScreen} />
+            <Stack.Screen name="Counter" component={CounterScreen} />
         </Stack.Navigator>
     );
 }
